a91fa2628adc74a963e7451d33817602f118d9af
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2017-07-20 15:28:26 +0200

    Replaced render :test to render :plain

--- master/app/controllers/job_monitoring_controller.rb
+++ master/app/controllers/job_monitoring_controller.rb
@@ -47,7 +47,7 @@ class JobMonitoringController < ApplicationController
   def print_log
     public_dir = File.expand_path('../../../public', __FILE__)
     text = @@workflow_manager.get_log(params[:job_id], :with_err)
-    render :plain => text.gsub(/\n/,'<br />')
+    render :plain => text
   end
   def print_script
     text = 'no script found'
@@ -58,7 +58,7 @@ class JobMonitoringController < ApplicationController
     else
       text = @@workflow_manager.get_script(params[:job_id])
     end
-    render :plain => text.gsub(/\n/,'<br />')
+    render :plain => text
   end
   def kill_job
     @status = 'kill job failed'
